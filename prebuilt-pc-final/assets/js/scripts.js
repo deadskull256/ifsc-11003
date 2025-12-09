5e06aae7c64514f1abdbe7bd93c28adbecfb7774
deadskull256 <mathewdownie1234@gmail.com>
2025-12-09 05:25:01 -0600

    Final Project Finished 1/2

--- prebuilt-pc-final/assets/js/scripts.js
+++ prebuilt-pc-final/assets/js/scripts.js
@@ -1,19 +1,31 @@
-$(document).ready(function() {
-
-    $('.fade').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        slide: 'div',
-        cssEase: 'linear',
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+$(document).ready(function () {
+  $(".fade").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slide: "div",
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
-
 
 function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function (){
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark"){
+    document.body.classList.add("dark-mode");
+  }
+});
